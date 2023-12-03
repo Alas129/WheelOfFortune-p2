f4117ef6980e930ab1006e46ca7d892280ed3644
alas129 <alas129@163.com>
2023-12-03 08:24:48 -0800

    wrong user

--- WheelOfFortune-SpringBoot/src/main/java/com/example/demo/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo;
-
-import com.google.cloud.spring.data.datastore.repository.DatastoreRepository;
-
-
-// Interface extending DatastoreRepository for User entity with String as the ID type
-public interface UserRepository extends DatastoreRepository<User, String> {
-}
