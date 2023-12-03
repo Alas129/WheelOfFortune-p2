559b65313af2b1f142c33bd979e6b31ae79788a6
jiaominye <yejiaomin@gmail.com>
2023-12-03 14:15:34 -0800

    Add new function to guess the whole phrase

--- WheelOfFortune-React/src/Game.js
+++ WheelOfFortune-React/src/Game.js
@@ -39,13 +39,14 @@ function Game() {
   const[hiddenPhrase,setHiddenPhrase]= useState(generateHiddenPhrase(secret));
   const[notice, setNotice] = useState("");
   const[guessLetter, setGuessLetter] = useState("");
+  const[guessPhrase, setGuessPhrase] = useState("");
   const[guessTime, setGuessTime] = useState(maxGuessingTime);
   const[previousGuess, setPreviousGuess] = useState("");
   const[gameOver,setGameOver] = useState(false);
   const {playerName} = useContext(PlayerNameContext);
   
-  // Event handler for input change
-  function handleChange(event){
+  // Event handler for input letter
+  function handleInputLetter(event){
     if(event.nativeEvent.data){
       setGuessLetter(event.nativeEvent.data.toLocaleLowerCase());
     }else{
@@ -53,32 +54,17 @@ function Game() {
     }
   }
 
-  //Event handler to start a new game
-  function handleStartNewGame(){
-    const newSecretIndex = Math.floor(Math.random() * phrases.length);
-    const newSecret = phrases[newSecretIndex].toLowerCase();
-    setHiddenPhrase(generateHiddenPhrase(newSecret));
-    setNotice("");
-    setGuessLetter("");
-    setGuessTime(maxGuessingTime);
-    setPreviousGuess("");
-    setGameOver(false);
-    wrongGuess = 0;
-    score = 0;
-  }
-
-  //Event handler to show the ranking page
-  function handleShowRanking(){
-    window.location.href = '/ranking';
-  }
-
-  //Event handler to edit playerName
-  function editPlayerName(){
-    window.location.href = '/playerName';
+   // Event handler for input phrase
+  function handleInputphrase(event){
+    if(event.target.value){
+      setGuessPhrase(event.target.value.toLocaleLowerCase());
+    }else{
+      setGuessPhrase("");
+    }
   }
 
   //Event handler to check the guessed letter
-  function checkGuess(){
+  function checkGuessLetter(){
     console.log(`playerNameCheck: ${playerName}`);
     if(gameOver){
       return;
@@ -99,6 +85,8 @@ function Game() {
         console.log(`hidden phrase: ${newHiddenPhrase}`);
         
         if(newHiddenPhrase === secret){
+          calculateScore();
+          alert("You game score is: " + score);
           setNotice("You won the game");
           winAudio2s.play();
           setGameOver(true);
@@ -123,6 +111,23 @@ function Game() {
       setGuessLetter("");
   }
 
+  function checkGuessPhrase(){
+    if(gameOver){
+      return;
+    }
+    if(guessPhrase===secret){
+      calculateScore();
+      setNotice("You won the game");
+      alert("You game score is: " + score);
+      winAudio2s.play();
+      setGameOver(true);
+    }else{
+      wrongGuess++;
+      loseAudio2s.play();
+      setNotice("You loss the game");
+      setGuessTime(prevGuessTime => prevGuessTime - 1);
+    }
+  }
   //Event handler to get hint
   function getHint(){
     let hint = '';
@@ -151,11 +156,14 @@ function Game() {
     window.location.href = '/';
   }
 
+  //funtion to calculate the score for every game
+  async function calculateScore(){
+    score = ((maxGuessingTime - wrongGuess) *100 / maxGuessingTime); 
+    console.log(`wrong guess: ${wrongGuess} ,axGuessingTime:${maxGuessingTime}, processing: ${(maxGuessingTime - wrongGuess)} ,score:${score}` )
+  }
   //Function to save the game to the backend
   async function saveGame(){  
     console.log(gameOver);
-    score = ((maxGuessingTime - wrongGuess) *100 / maxGuessingTime); 
-    console.log(`wrong guess: ${wrongGuess} ,axGuessingTime:${maxGuessingTime}, processing: ${(maxGuessingTime - wrongGuess)} ,score:${score}` )
     const postData = {
         userId:auth.currentUser.email,
         playerName:playerName,
@@ -168,6 +176,31 @@ function Game() {
     }
   }
 
+  //Event handler to show the ranking page
+  function handleShowRanking(){
+    window.location.href = '/ranking';
+  }
+
+  //Event handler to edit playerName
+  function editPlayerName(){
+    window.location.href = '/playerName';
+  }
+
+
+  //Event handler to start a new game
+  function handleStartNewGame(){
+    const newSecretIndex = Math.floor(Math.random() * phrases.length);
+    const newSecret = phrases[newSecretIndex].toLowerCase();
+    setHiddenPhrase(generateHiddenPhrase(newSecret));
+    setNotice("");
+    setGuessLetter("");
+    setGuessTime(maxGuessingTime);
+    setPreviousGuess("");
+    setGameOver(false);
+    wrongGuess = 0;
+    score = 0;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -177,10 +210,15 @@ function Game() {
         <h3>{guessTime} times to guess!</h3>
       </div>
       <div className = "userInput">
-        <label>Guessig the phrase </label>
-        <input type= "text" maxLength={1} value = {guessLetter} placeholder='letter' onChange = {handleChange}/>
-        <button onClick={checkGuess} disabled={gameOver}>Submit</button>
+        <label>Guessig Single Letter </label>
+        <input type= "text" maxLength={1} value = {guessLetter} placeholder='letter' onChange = {handleInputLetter}/>
+        <button onClick={checkGuessLetter} disabled={gameOver}>Submit</button>
         <button onClick={getHint} >Two chances for one hint</button>
+      </div>
+      <div className = "userInput">
+        <label>Guessig Whole Phrase </label>
+        <input type= "text" value = {guessPhrase} placeholder='phrase' onChange = {handleInputphrase}/>
+        <button onClick={checkGuessPhrase} disabled={gameOver}>Submit</button>
       </div>
       <div>
         <h2>{notice}</h2>{gameOver&&
